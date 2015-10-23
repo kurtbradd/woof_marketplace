177eee5c68865ece8705b80b81f05977525d9475
Kurt DaCosta <kurtbradd@gmail.com>
2015-10-23 00:59:17 -0400

    implemented skeleton User & Session controllers

---
+++ routes/controllers/SessionsCtrl.js
@@ -0,0 +1,5 @@
+module.exports = {
+	register: function register (req, res, next) { return res.send(200); },
+	login: 		function login 		(req, res, next) { return res.send(200); },
+	logout: 	function logout 	(req, res, next) { return res.send(200); }
+}
---
+++ routes/controllers/UserCtrl.js
@@ -0,0 +1,7 @@
+module.exports = {
+	getUserProfile: function getUserProfile (req, res, next) { return res.send(200); },
+	getMyUserProfile: function getMyUserProfile (req, res, next) { return res.send(200); },
+	updateUserProfile: function updateUserProfile (req, res, next) { return res.send(200); },
+	updateUserPassword: function updateUserPassword (req, res, next) { return res.send(200); },
+	updateUserProfilePicture: function updateUserProfilePicture (req, res, next) { return res.send(200); }	
+}
