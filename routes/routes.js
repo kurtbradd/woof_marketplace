c7f3ccc8422698cfaa580a006d54975554b464aa
Kurt DaCosta <kurtbradd@gmail.com>
2015-11-03 21:25:01 -0500

    added Listing API routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var UserCtrl = require('./controllers/UserCtrl');
-var SessionsCtrl = require('./controllers/SessionsCtrl');
+var SessionCtrl = require('./controllers/SessionCtrl');
+var ListingCtrl = require('./controllers/ListingCtrl');
 
 module.exports = function (server) {
 
@@ -10,10 +11,10 @@ module.exports = function (server) {
   });
 
   // SESSIONS
-  var sessionsEndpoint = '/sessions';
-  server.post(sessionsEndpoint + '/register', SessionsCtrl.register);
-  server.post(sessionsEndpoint + '/login', SessionsCtrl.login);
-  server.get (sessionsEndpoint + '/logout', SessionsCtrl.logout);
+  var sessionsEndpoint = '/session';
+  server.post(sessionsEndpoint + '/register', SessionCtrl.register);
+  server.post(sessionsEndpoint + '/login', SessionCtrl.login);
+  server.get (sessionsEndpoint + '/logout', SessionCtrl.logout);
 
   // USER
   var userEndpoint = '/user';
@@ -23,5 +24,19 @@ module.exports = function (server) {
   server.post(userEndpoint + '/password', UserCtrl.updateUserPassword);
   server.post(userEndpoint + '/profile', UserCtrl.updateUserProfile);
 
+  // LISTING
+  var listingEndpoint = '/listing';
+  server.get (listingEndpoint + '/', ListingCtrl.getListings);
+  server.post(listingEndpoint + '/',ListingCtrl.createListing);
+  server.get (listingEndpoint + '/:listing_id',ListingCtrl.getListing);
+  server.post(listingEndpoint + '/:listing_id',ListingCtrl.updateListing);
+  server.del (listingEndpoint + '/:listing_id',ListingCtrl.deleteListing);
+
+  // REVIEW
+  var reviewEndpoint = '/review';
+
+  // TRANSACTION
+  var transaction = '/'
+  
   console.log('Routes loaded');
 }
