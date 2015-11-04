cd1495456e1c4a7b69125e8099d54ff59adb8e08
Kurt DaCosta <kurtbradd@gmail.com>
2015-11-03 21:23:56 -0500

    implemented listing and review controller scaffolding

---
+++ routes/controllers/ListingCtrl.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+
+	getListings: function getListings (req, res, next) {
+		return res.send(200);
+	},
+
+	getListing: function getListing (req, res, next) {
+		return res.send(200);
+	},
+
+	createListing: function createListing (req, res, next) {
+		return res.send(200);
+	},
+
+	updateListing: function updateListing (req, res, next) {
+		return res.send(200);
+	},
+
+	deleteListing: function deleteListing (req, res, next) {
+		return res.send(200);
+	} 
+
+
+
+}
---
+++ routes/controllers/ReviewCtrl.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+
+	// get reviews
+	// create review
+	// delete review
+
+
+	deletereview: function deletereview (req, res, next) {},
+
+
+}
