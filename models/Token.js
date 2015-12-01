6f6dc56b490c2265f9a9a6f75a154b891c97af56
Kurt DaCosta <kurtbradd@gmail.com>
2015-11-30 20:53:00 -0500

    token model schema

---
+++ models/Token.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+		var Token = sequelize.define('Token', {
+		user_id: {
+			primaryKey: true,
+			type: DataTypes.BIGINT,
+			references: { model: "Users", key: "id" }
+		},
+		token: {
+			primaryKey: true,
+			allowNull: false,
+			type: DataTypes.STRING
+		}
+	},{
+		tableName: 'Tokens',
+	});
+	return Token;
+};
