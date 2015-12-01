55423971521ad52cc70c56a27c9ff0e1286be94d
Kurt DaCosta <kurtbradd@gmail.com>
2015-11-30 20:57:56 -0500

    implemented Token create, verify, and delete fns

--- modules/TokenManager.js
+++ modules/TokenManager.js
@@ -1,32 +1,37 @@
+var db                = require('../models/index.js');
+var jwt               = require('jsonwebtoken');
+var restify           = require('restify');
 var sessionConfig     = require(__dirname + '/../config/secrets.js').sessions;
 var sessionSecret     = sessionConfig.secret;
 var sessionExpiration = sessionConfig.expiration;
-var jwt               = require('jsonwebtoken');
 
 exports.createToken = function (userId, cb) {
-  var token = jwt.sign({id: userId}, sessionSecret, {expiresInMinutes:sessionExpiration});
-  // storetoken for user
+  var token = jwt.sign({id: userId}, sessionSecret, {expiresIn: sessionExpiration});
+
+  db.Token.create({ user_id: userId, token: token }).then(function (jwt) {
+    if (!jwt) return cb(new restify.InternalError("Could Not Store Token"));
+    if (jwt) return cb(null, jwt.token);
+  }).catch(cb);
 }
 
 exports.verifyToken = function (req, res, next) {
   var token = req.authorization.credentials;
 
-  jwt.verify(token, sessionSecret, function(err, decoded) {
-    if (err) return res.send(401, {error:err}); //invalid token
-    // after verifying token, check for token in DB,
-    // checking if it has expired,
-    // if not expired, return the user_id as req.user, then call next();
+  jwt.verify(token, sessionSecret, function (err, decoded) {
+    if (err) return res.send(401, {error:err});
+
+    req.user = decoded.id;
+    var queryParams = { where: { user_id: req.user, token: token }};
+
+    db.Token.findOne(queryParams).then(function (foundToken) {
+      if (foundToken) return next();
+      return next(new restify.InvalidCredentialsError("Invalid Token"));
+    }).catch(next);
   });
 }
 
 exports.expireToken = function(userId, token, cb) {
-  // delete token from database
-}
-
-exports.deleteTokensForUser = function(userId, cb) {
-  // delete all tokens for user from db
-}
-
-var storeUserToken = function (userId, token, cb) {
-  // store new token for user
+  var queryParams = { where: { user_id: userId, token: token }};
+  db.Token.destroy(queryParams)
+  .then(function () { return cb(); }).catch(cb);
 }
