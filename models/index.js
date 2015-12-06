fd9d112c24f9e5f274ab3679ac825b433d46813a
Kurt DaCosta <kurtbradd@gmail.com>
2015-12-05 22:28:35 -0500

    async loading of db models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var fs        = require("fs");
 var path      = require("path");
+var async     = require('async');
 var Sequelize = require("sequelize");
 var config    = require(__dirname + '/../config/secrets.js').database.pg;
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
@@ -7,41 +8,47 @@ var db        = {};
 
 
 var isModel = function (file) {
-	var hasExtension 	= file.indexOf(".") !== 0;
-	var notIndex 			= file !== "index.js";
-	return hasExtension && notIndex;
+  var hasExtension  = file.indexOf(".") !== 0;
+  var notIndex      = file !== "index.js";
+  return hasExtension && notIndex;
 }
 
-var importModel = function (file) {
-	var file_path = path.join(__dirname, file);
-	var model = sequelize["import"](file_path);
-	db[model.name] = model;
+var importModel = function (file, cb) {
+  var file_path = path.join(__dirname, file);
+  var model = sequelize["import"](file_path);
+  db[model.name] = model;
+  return cb(null);
 }
 
-var associateModel = function (model_name) {
-	if ("associate" in db[model_name]) {
-		db[model_name].associate(db);
-	}
+var associateModel = function (model_name, cb) {
+  if ("associate" in db[model_name]) {
+    db[model_name].associate(db);
+  }
+  return cb(null);
 }
 
-var successHandler = function () {
-	console.log("Models Loaded");
+var loadModels = function (cb) {
+  async.waterfall([
+    function loadModelFiles (cb) {
+      return fs.readdir(__dirname, cb);
+    },
+    function filterModels (files, cb) {
+      return cb(null, files.filter(isModel));
+    },
+    function importModels (models, cb) {
+      return async.each(models, importModel, cb);
+    },
+    function associateModels (cb) {
+      return async.each(Object.keys(db), associateModel, cb);
+    },
+    function syncronizeSequelize (cb) {
+      sequelize.sync({force: true})
+      .then(function () { cb(null); }).catch(cb);
+    }
+  ], cb);
 }
 
-
-db.loadModels = function (cb) {
-	// Load & Import Data Models from definitions
-	fs.readdirSync(__dirname).filter(isModel).forEach(importModel);
-
-	// Associate Data Models with Sequelize Models
-	Object.keys(db).forEach(associateModel);
-
-	// Syncronize Database with Sequelize Models
-	sequelize.sync( { force: true } )
-	.then(function () { cb(null); })
-	.catch(cb);
-
-}
+db.loadModels = loadModels;
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
