ed4c5d850ce121d2ef0f099621b65c7578701b8f
Kurt DaCosta <kurtbradd@gmail.com>
2015-12-08 18:48:28 -0500

    add authentication interceptor to angular app

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var app = angular.module('app', ['ui.router']);
 app.run(['$rootScope', function ($rootScope) {
   $rootScope._ = window._;
 }]);
+
+app.config(function ($httpProvider) {
+	$httpProvider.interceptors.push('AuthInterceptor')
+})
