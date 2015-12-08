9076b0c17701c8e5467d0b07934e8b0ee2ffc615
Kurt DaCosta <kurtbradd@gmail.com>
2015-12-08 18:45:35 -0500

    implemented session api functionality

--- public/js/main/services/SessionService.js
+++ public/js/main/services/SessionService.js
@@ -5,6 +5,37 @@ app.factory('SessionService', sessionService);
 sessionService.$inject = ['$http'];
 
 function sessionService ($http) {
+	var self = this;
 	var service = {};
+	var currentUser = {};
+	var api = '/session';
+
+	service.register = function (form, cb) {
+		$http.post(api + '/register', form).then(function (res) {
+			return cb(null, res.data);
+		}, cb);
+	}
+
+	service.login = function (email, password, cb) {
+		var params = {email: email, password: password};
+		$http.post(api + '/login', params).then(function (res) {
+			return cb(null, res.data);
+		}, cb);
+	}
+
+	service.logout = function (cb) {
+		$http.get(api + '/logout').then(function (res) {
+			return cb(null, res.data);
+		}, cb);
+	}
+
+	service.getCurrentUser = function () {
+		return currentUser;
+	}
+
+	service.setCurrentUser = function (user) {
+		currentUser = user;
+	}
+
 	return service;
 }
