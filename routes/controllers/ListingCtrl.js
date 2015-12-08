73d8d2d4115e0404e3f78ae46d4c055f23b24004
Kurt DaCosta <kurtbradd@gmail.com>
2015-12-08 13:16:49 -0500

    implented CRUD for listing model

--- routes/controllers/ListingCtrl.js
+++ routes/controllers/ListingCtrl.js
@@ -1,12 +1,38 @@
+var restify = require('restify');
+var async = require('async');
+var db = require('../../models/index.js');
+var _ = require('lodash');
+
 module.exports = {
 
 
 	getListings: function getListings (req, res, next) {
-		return res.send(200);
+		db.Listing.findAll({
+			include: [{model: db.ListingImage, as: 'images'}]
+		}).then(function (listings) {
+			return res.send(200, {listings: listings});
+		}).catch(next);
 	},
 
 	getListing: function getListing (req, res, next) {
-		return res.send(200);
+		var listing_id = req.params.listing_id;
+		if (!listing_id) return next(new restify.MissingParameterError('Missing Listing Id'));
+
+		db.Listing.findOne({where: {listing_id: listing_id}})
+		.then(function (listing) {
+			return res.send(200, {listing: listing});
+		}).catch(next);
+	},
+
+	getListingImages: function getListingImages (req, res, next) {
+		var listing_id = req.params.listing_id;
+		if (!listing_id) return next(new restify.MissingParameterError('Missing Listing Id'));
+
+		db.ListingImage.findAll({where: {listing_id: listing_id}})
+		.then(function (listing_images) {
+			return res.send(200, {listing_images: listing_images})
+			.catch(next);
+		})
 	},
 
 	createListing: function createListing (req, res, next) {
