54a588091c91920d6e8a7f427c5d000acb14ff9f
Kurt DaCosta <kurtbradd@gmail.com>
2015-12-08 18:47:08 -0500

    implemented signup functionality

--- public/js/main/controllers/SignUpCtrl.js
+++ public/js/main/controllers/SignUpCtrl.js
@@ -1,5 +1,17 @@
+'use strict';
+
 app.controller('SignUpCtrl', signupCtrl);
 
-signupCtrl.$inject = ['$scope', '$stateParams', '$state'];
+signupCtrl.$inject = ['$scope', '$state', 'SessionService'];
 
-function signupCtrl ($scope, $stateParams, $state) {}
+function signupCtrl ($scope, $state, SessionService) {
+
+	$scope.form = {};
+
+	$scope.submitForm = function () {
+		SessionService.register($scope.form, function (err, user) {
+			if (err) return console.error(err);
+			console.log(user);
+		});
+	}
+}
